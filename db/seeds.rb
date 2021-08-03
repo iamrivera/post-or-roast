bb3c1b06c45c44c5a668b6aaf45c8ce3d724933f
iamrivera <a3m3rivera@gmail.com>
2021-08-03 00:17:07 -0700

    Creating Genre Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# CREATING GENRES
+genres = Genre.create([{name: 'Horror'}, {name: 'Action'}, {name: 'Science Fiction'}])
